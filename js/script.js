619dfe2fcc9dc2fc21f7aa20407341c9ae9ca3f1
rus733 <9282716617@mail.ru>
2021-12-18 20:03:08 +0300

    создание элемента1

--- js/script.js
+++ js/script.js
@@ -8,88 +8,22 @@ const DomElement = function (selector, height, width, bg, fontSize) {
   this.bg = bg;
   this.fontSize = fontSize;
 
-  createElement() {},
-};
-
-const dom1 = new DomElement();
-
-//
-//
-//
-//
-
-/*
-const todoControl = document.querySelector('.todo-control');
-const headerInput = document.querySelector('.header-input');
-const todoList = document.querySelector('.todo-list');
-const todoCompleted = document.querySelector('.todo-completed');
-const todoRemove = document.querySelector('.todo-remove');
-
-let toDoData = [];
-
-const render = function () {
-  todoList.innerHTML = '';
-  todoCompleted.innerHTML = '';
-  toDoData.forEach(function (item, index) {
-    const li = document.createElement('li');
-    li.classList.add('todo-item');
-    li.innerHTML =
-      '<span class="text-todo">' +
-      item.text +
-      '</span>' +
-      '<div class="todo-buttons">' +
-      '<button class="todo-remove"></button>' +
-      '<button class="todo-complete"></button>' +
-      '</div>';
-
-    if (item.completed) {
-      todoCompleted.append(li);
-    } else {
-      todoList.append(li);
+  this.createElement = function () {
+    //console.log(selector[0]);
+    if (this.selector[0] === '.') {
+      const div = document.createElement('div');
+      //console.log(this.selector.slice(1));
+      div.className = this.selector.slice(1);
+      div.style.cssText = `height: ${this.height};
+    width: ${this.width};
+    background:  ${this.bg};
+    font-size: ${this.fontSize};
+  `;
+      document.body.append(div);
     }
-
-    li.querySelector('.todo-complete').addEventListener('click', function () {
-      item.completed = !item.completed;
-      render();
-      addToStorage();
-    });
-    // удалениe  задач
-    li.querySelector('.todo-remove').addEventListener('click', function () {
-      toDoData.splice(index, 1);
-      render();
-      addToStorage();
-    });
-  });
+  };
 };
 
-todoControl.addEventListener('submit', function (event) {
-  event.preventDefault();
+const newElement1 = new DomElement('.class', '100px', '200px', 'red', '16px');
 
-  if (headerInput.value.trim() !== '') {
-    const newToDo = {
-      text: headerInput.value,
-      completed: false,
-    };
-    toDoData.push(newToDo);
-    headerInput.value = '';
-    render();
-  } else {
-    alert('заполните поле ввода задачи');
-    headerInput.value = '';
-  }
-  addToStorage();
-});
-
-//функция добавления данных в локальное хранилище
-const addToStorage = function () {
-  localStorage.clear();
-  localStorage.setItem('todo', JSON.stringify(toDoData));
-};
-
-// проверка  локального хранилища и занесение данных в массив
-
-if (JSON.parse(localStorage.getItem('todo')) !== null) {
-  toDoData = JSON.parse(localStorage.getItem('todo'));
-  render();
-}
-*/
+newElement1.createElement();
