51726abad781dbd862010cb4e6e3e0276b226332
rus733 <9282716617@mail.ru>
2021-12-18 20:56:22 +0300

    задание 15 для проверки

--- js/script.js
+++ js/script.js
@@ -9,24 +9,21 @@ const DomElement = function (selector, height, width, bg, fontSize) {
   this.fontSize = fontSize;
 
   this.createElement = function () {
-    //console.log(selector[0]);
     if (this.selector[0] === '.') {
       const div = document.createElement('div');
-      //console.log(this.selector.slice(1));
       div.className = this.selector.slice(1);
-      div.style.cssText = `height: ${this.height};
-          width: ${this.width};
-          background:  ${this.bg};
-          font-size: ${this.fontSize};`;
-
-      div.textContent = prompt('Сюда можно ввести любой текст', ' Это Элемент1 с построенный на основе класса    ');
+      div.style.cssText = `height: ${this.height}; width: ${this.width}; 
+      background:  ${this.bg}; font-size: ${this.fontSize};`;
+      div.textContent = 'Блок созданный на основе класса .block';
+      //второй вариант  ввода текста в элементы
+      //div.textContent = prompt('Сюда можно ввести любой текст', ' Это Элемент1 с построенный на основе класса    ');
       document.body.append(div);
     } else if (this.selector[0] === '#') {
       const p = document.createElement('p');
       p.id = this.selector.slice(1);
       p.style.cssText = `height: ${this.height}; width: ${this.width};
-       background: ${this.bg}; font-size: ${this.fontSize};`;
-      p.textContent = 'Параграф созданный с ID';
+      background: ${this.bg}; font-size: ${this.fontSize};`;
+      p.textContent = 'Параграф созданный на основе #id';
       document.body.append(p);
     }
   };
@@ -34,5 +31,6 @@ const DomElement = function (selector, height, width, bg, fontSize) {
 
 const newElement1 = new DomElement('.block', '200px', '400px', 'red', '32px');
 const newElement2 = new DomElement('#best', '200px', '400px', 'green', '32px');
+
 newElement1.createElement();
 newElement2.createElement();
