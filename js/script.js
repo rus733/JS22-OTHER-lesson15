d86b1ee56228262142d8461eab5740892563cdd9
rus733 <9282716617@mail.ru>
2021-12-18 20:49:39 +0300

    создан элемент2

--- js/script.js
+++ js/script.js
@@ -18,11 +18,21 @@ const DomElement = function (selector, height, width, bg, fontSize) {
           width: ${this.width};
           background:  ${this.bg};
           font-size: ${this.fontSize};`;
+
+      div.textContent = prompt('Сюда можно ввести любой текст', ' Это Элемент1 с построенный на основе класса    ');
       document.body.append(div);
+    } else if (this.selector[0] === '#') {
+      const p = document.createElement('p');
+      p.id = this.selector.slice(1);
+      p.style.cssText = `height: ${this.height}; width: ${this.width};
+       background: ${this.bg}; font-size: ${this.fontSize};`;
+      p.textContent = 'Параграф созданный с ID';
+      document.body.append(p);
     }
   };
 };
 
-const newElement1 = new DomElement('.class', '100px', '200px', 'red', '16px');
-
+const newElement1 = new DomElement('.block', '200px', '400px', 'red', '32px');
+const newElement2 = new DomElement('#best', '200px', '400px', 'green', '32px');
 newElement1.createElement();
+newElement2.createElement();
