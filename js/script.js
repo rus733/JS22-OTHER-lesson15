e76877f1490205907775c33abf5c9ccc4678eedc
rus733 <9282716617@mail.ru>
2021-12-20 12:08:35 +0300

    задание 15 вер 2

--- js/script.js
+++ js/script.js
@@ -1,36 +1,33 @@
 'use strict';
 // дз 15
 
-const DomElement = function (selector, height, width, bg, fontSize) {
+const DomElement = function (selector, height, width, bg, fontSize, text) {
   this.selector = selector;
   this.height = height;
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
+  this.text = text;
+  const newElement = this.newElement;
 
-  this.createElement = function () {
+  this.createElement = () => {
+    let elementType;
     if (this.selector[0] === '.') {
-      const div = document.createElement('div');
-      div.className = this.selector.slice(1);
-      div.style.cssText = `height: ${this.height}; width: ${this.width}; 
-      background:  ${this.bg}; font-size: ${this.fontSize};`;
-      div.textContent = 'Блок созданный на основе класса .block';
-      //второй вариант  ввода текста в элементы
-      //div.textContent = prompt('Сюда можно ввести любой текст', ' Это Элемент1 с построенный на основе класса    ');
-      document.body.append(div);
+      elementType = 'div';
     } else if (this.selector[0] === '#') {
-      const p = document.createElement('p');
-      p.id = this.selector.slice(1);
-      p.style.cssText = `height: ${this.height}; width: ${this.width};
-      background: ${this.bg}; font-size: ${this.fontSize};`;
-      p.textContent = 'Параграф созданный на основе #id';
-      document.body.append(p);
+      elementType = 'p';
     }
+    this.newElement = document.createElement(elementType);
+    this.newElement.classList.add(this.selector);
+    this.newElement.style.cssText = `height: ${this.height}; width: ${this.width};
+     background: ${this.bg}; font-size: ${this.fontSize};`;
+    this.newElement.textContent = this.text;
+    document.body.append(this.newElement);
   };
 };
 
-const newElement1 = new DomElement('.block', '200px', '400px', 'red', '32px');
-const newElement2 = new DomElement('#best', '200px', '400px', 'green', '32px');
+const newElement1 = new DomElement('.block', '100px', '400px', 'red', '32px', 'Это Блок с классом "');
+const newElement2 = new DomElement('#best', '100px', '400px', 'green', '32px', 'Это параграф с id');
 
 newElement1.createElement();
 newElement2.createElement();
